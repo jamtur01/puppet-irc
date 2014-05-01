10fedcbd14c92efb913db4801a134bb50e5e3dff
Adam Crews <adam@puppetlabs.com>
2014-05-01 15:47:48 -0700

    Refactor to params style

--- spec/classes/irc_spec.rb
+++ spec/classes/irc_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe 'irc' do
-  it { should contain_package('carrier-pigeon').with_provider('gem') }
+  context 'on Puppet OpenSource' do
+    let(:params) { { 
+      :irc_server      => 'irc.example.org',
+      :use_ssl         => false,
+      :register_first  => false,
+      :show_joins      => false,
+      :puppet_confdir  => '/etc/puppet',
+      :gem_provider    => 'gem'
+    } }
+
+    it { should contain_package('carrier-pigeon').with_provider('gem') }
+  end
 
   context 'on Puppet Enterprise' do
-    let(:facts) { {:is_pe => true} }
+    let(:params) { { 
+      :irc_server      => 'irc.example.org',
+      :use_ssl         => false,
+      :register_first  => false,
+      :show_joins      => false,
+      :puppet_confdir  => '/etc/puppetlabs/puppet',
+      :gem_provider    => 'pe_gem'
+    } }
 
     it { should contain_package('carrier-pigeon').with_provider('pe_gem') }
   end
