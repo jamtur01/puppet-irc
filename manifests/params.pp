3fa9eeee637968281f47cdf0ce43d8efac6b8c27
Adam Crews <adam@puppetlabs.com>
2014-05-01 15:57:27 -0700

    Fix spec and params inheritance

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,22 @@
 class irc::params {
 
-  $irc_server         = undef,
-  $use_ssl            = false,
-  $channel_password   = undef,
-  $register_first     = false,
-  $show_joins         = true,
-  $timeout            = undef,
-  $github_user        = undef,
-  $github_password    = undef,
-  $parsed_reports_dir = undef,
-  $report_url         = undef,
+  $irc_server         = undef
+  $use_ssl            = false
+  $channel_password   = undef
+  $register_first     = false
+  $show_joins         = true
+  $timeout            = undef
+  $github_user        = undef
+  $github_password    = undef
+  $parsed_reports_dir = undef
+  $report_url         = undef
 
-  if $::is_pe { 
-    $puppet_user        = 'pe-puppet',
+  if $::is_pe {
+    $puppet_user        = 'pe-puppet'
     $puppet_confdir     = '/etc/puppetlabs/puppet'
     $gem_provider       = 'pe_gem'
-  } else { 
-    $puppet_user        = 'puppet',
+  } else {
+    $puppet_user        = 'puppet'
     $puppet_confdir     = '/etc/puppet'
     $gem_provider       = 'gem'
   }
